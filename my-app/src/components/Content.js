ceb91c358790860d5154650c9e9455a59e2fbc20
adamhatton <adamdhatton@gmail.com>
2022-09-17 15:02:16 +0000

    Add rendering lists challenge

---
+++ my-app/src/components/Content.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import css from "./css/Content.module.css"
+import {savedPosts} from "../posts.json"
+import PostItem from './PostItem'
+
+class Content extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className={css.Content}>
+        <div className={css.TitleBar}>
+            <h1>My Posts</h1>
+        </div>
+        <div className={css.SearchResults}>
+            <PostItem posts={savedPosts}/>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Content
