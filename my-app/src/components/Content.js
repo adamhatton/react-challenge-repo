14396f664e923208f063aea85333d7eac4b1c982
adamhatton <adamdhatton@gmail.com>
2022-09-17 17:06:25 +0000

    Add lifecycle challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,20 +2,34 @@ import React, { Component } from 'react'
 import css from "./css/Content.module.css"
 import {savedPosts} from "../posts.json"
 import PostItem from './PostItem'
+import Loader from './Loader'
 
 class Content extends Component {
   constructor(props) {
     super(props)
+  
+    this.state = {
+       isLoaded: false
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        isLoaded: true
+      })
+    }, 2000)
   }
 
   render() {
     return (
+
       <div className={css.Content}>
         <div className={css.TitleBar}>
             <h1>My Posts</h1>
         </div>
         <div className={css.SearchResults}>
-            <PostItem posts={savedPosts}/>
+          {this.state.isLoaded === false ? (<Loader />) : <PostItem posts={savedPosts}/>}
         </div>
       </div>
     )
