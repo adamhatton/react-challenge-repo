1d18fd7e79f03feee5e6b20614263a6e7809952f
adamhatton <adamdhatton@gmail.com>
2022-09-19 13:04:15 +0000

    Add search filter challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -9,16 +9,28 @@ class Content extends Component {
     super(props)
   
     this.state = {
-       isLoaded: false
+       isLoaded: false,
+       posts: []
     }
   }
 
   componentDidMount() {
-    setInterval(() => {
+    setTimeout(() => {
       this.setState({
-        isLoaded: true
+        isLoaded: true,
+        posts: savedPosts
       })
     }, 2000)
+  }
+
+  handleChange = (event) => {
+    const name = event.target.value.toLowerCase()
+    const filteredPosts = savedPosts.filter((post) => {
+      return post.name.toLowerCase().includes(name)
+    })
+    this.setState({
+      posts: filteredPosts,
+    })
   }
 
   render() {
@@ -27,9 +39,20 @@ class Content extends Component {
       <div className={css.Content}>
         <div className={css.TitleBar}>
             <h1>My Posts</h1>
+            <form>
+              <label htmlFor='searchinput'>Search:</label>
+              <input
+                type='search'
+                id='searchinput'
+                placeholder='By Author'
+                onChange={this.handleChange}
+              />
+              <h4>posts found: {this.state.posts.length}</h4>              
+            </form>
+
         </div>
         <div className={css.SearchResults}>
-          {this.state.isLoaded === false ? (<Loader />) : <PostItem posts={savedPosts}/>}
+          {this.state.isLoaded === false ? (<Loader />) : <PostItem posts={this.state.posts}/>}
         </div>
       </div>
     )
